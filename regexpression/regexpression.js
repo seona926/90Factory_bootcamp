1349cb9575c3ba55913323036befda843af7c898
Sophia <suna9267@gmail.com>
2021-03-06 20:29:38 +0900

    modify all structure

--- regexpression/regexpression.js
+++ regexpression/regexpression.js
@@ -1,21 +1,8 @@
-
 function fcnOnclick() {
     document.getElementById("result").innerHTML ='주민등록번호: ';
-    let idValue = document.getElementById('identification').value;
-    let arrID = [];
-    let resultID = '';
-    let reg = /\d{2}([0]\d|[1][0-2])([0][1-9]|[1-2]\d|[3][0-1])[-]*[1-4]\d{6}/g;
-    if(reg.test(idValue)) {
-        arrID = idValue.split("");
-        for (let i = 0; i < arrID.length; i++) {
-            if (i <= 6) {
-                resultID += arrID[i];
-            } else if (i >= 7) {
-                resultID += '*';
-            }
-        }
-        document.getElementById("result").innerHTML += resultID;
-    } else {
-        document.getElementById("result").innerHTML += '유효한 주민등록번호가 아닙니다.';
-    }
+    let inputText = document.getElementById('identification').value;
+    const regexId = /\d{2}([0]\d|[1][0-2])([0][1-9]|[1-2]\d|[3][0-1])[-]*[1-4]\d{6}/g;
+    const id = inputText.match(regexId).join("");
+    const resultId =id.replace(/(?<=)\d{6}$/g, '******')
+    document.getElementById("result").innerHTML += resultId;
 }
