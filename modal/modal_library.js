5cad1993af92d00e7670db1d4751f3123c5d37ff
Sophia <suna9267@gmail.com>
2021-03-18 19:59:31 +0900

    make modal library

---
+++ modal/modal_library.js
@@ -0,0 +1,17 @@
+(function(){
+    const openButton = document.getElementById("open");
+    const modal = document.querySelector(".modal");
+    const overlay = modal.querySelector(".modal_bg");
+    const closeBtn = modal.querySelector("button");
+
+    const openModal = () => {
+        modal.classList.remove("hidden");
+    }
+    const closeModal = () => {
+        modal.classList.add("hidden");
+    }
+
+    overlay.addEventListener("click", closeModal);
+    closeBtn.addEventListener("click", closeModal);
+    openButton.addEventListener("click", openModal);
+}());
