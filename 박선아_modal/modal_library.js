63b2d99ef2666101dfd9f6497546b2743e268cc7
Sophia <suna9267@gmail.com>
2021-03-23 22:08:56 +0900

    modify modal library

--- 박선아_modal/modal_library.js
+++ 박선아_modal/modal_library.js
@@ -1,23 +1,24 @@
 function makeModal(className, modalContent){ 
-    const openButton = document.getElementById("open"); 
+    // 모달창 만들기
     const modal = document.createElement("div"); 
     const modalBg = document.createElement("div"); 
     const h1 = document.createElement("h1"); 
     const content = document.createElement("div");
     const closeBtn = document.createElement("button");
-
- 
     const customTextcontainer = document.createElement("p");
 
+    // TextNodes
     const hlTextNode = document.createTextNode("Hello World!!"); 
     const contentTextNode = document.createTextNode(modalContent); 
     const closeTextNode = document.createTextNode('Close');
+    const modalBtnText = document.getElementsByClassName(className);
+
+    // 모달창 요소에 클래스이름 짓기
     modal.className = 'modal'; 
     modalBg.className = 'modal_bg'; 
     content.className = 'modal_content';
 
-
-
+    // 모달 창 요소에 textNodes 연결하기, 요소 append하기
     h1.appendChild(hlTextNode);
     content.appendChild(h1);
     
@@ -40,10 +41,8 @@ function makeModal(className, modalContent){
 
     modalBg.addEventListener("click", closeModal); 
     closeBtn.addEventListener("click", closeModal); 
-    openButton.addEventListener("click", openModal);
+    className.addEventListener("click", openModal);
 
  
-    
-
     document.body.appendChild(modal);
 }
